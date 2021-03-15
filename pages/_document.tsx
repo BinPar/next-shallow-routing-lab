23768200f358cb9813e972aa9205a0642403d8f6
Ignacio Ferro Picón <nacho@binpar.com>
2021-03-15 12:24:36 +0100

    Added Document Page

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentInitialProps,
+} from 'next/document';
+
+class MyDocument extends Document {
+
+  public static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+
+  public render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" type="text/css" href="/styles.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
